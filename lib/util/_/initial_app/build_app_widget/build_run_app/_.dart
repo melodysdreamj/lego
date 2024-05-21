17e279260de436b6825af783c4c15a8851b4f0f3
june lee <melodysdreamj@gmail.com>
2024-05-21 09:48:07 +0900

    Update _.dart

--- lib/util/_/initial_app/build_app_widget/build_run_app/_.dart
+++ lib/util/_/initial_app/build_app_widget/build_run_app/_.dart
@@ -3,7 +3,8 @@ import '../../../../../main.dart';
 import '../../ready_functions/before_run_app/_.dart';
 import '../build_my_app/_.dart';
 
-Future<void> buildApp() async {
+Future<void> buildApp({Widget? home}) async {
+  MyAppHome = home;
   await readyBeforeRunApp();
   Widget childWidget = MyApp();
 
